e6233156c5c9fdf0c308fb375c3aa075a56ac867
Shiv Toolsidass <shivtoolsidass@gmail.com>
2016-03-21 16:45:37 -0400

    modified server.js

--- server.js
+++ server.js
@@ -1,4 +1,22 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
-console.log("Connected to 8080. Serve up by going to localhost:8080/index.html");
+var express = require('express');
+var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname));
+
+// set the home page route
+app.get('/', function(req, res) {
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
